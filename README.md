9d7241086c225d76ebc14de9d6a9894b290b190b
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-11-07 12:46:20 +0530

    SpringBoot Spring Data JPA CRUD operation REST API

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot-SpringDataJPA-CRUD-REST-API
---
+++ src/test/java/com/gl/EmployeeManagementApplication/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.EmployeeManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
